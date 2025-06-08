11882758acf4c4c0441644739723c3a9dac91c1b
Mirella230 <mirellaaparecida806@gmail.com>
2025-06-08 19:46:38 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -2,3 +2,28 @@ function alterarTexto() {
     document.getElementById("titulo").innerText = "Texto Alterado!";
     document.getElementById("titulo").classList.add("destaque");
     }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('loginForm');
+
+ 
+  const usuarioCadastrado = {
+    email: 'teste@exemplo.com',
+    senha: '1234'
+  };
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const emailDigitado = document.getElementById('email').value;
+    const senhaDigitada = document.getElementById('senha').value;
+
+   
+    if (emailDigitado === usuarioCadastrado.email && senhaDigitada === usuarioCadastrado.senha) {
+      alert('Login bem-sucedido!');
+     window.location.href='pagina-principal.html';
+    } else {
+      alert('E-mail ou senha inválidos.');
+    }
+  });
+});
